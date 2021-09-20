4dd17b309f579962863b42b3d1d54d0397a385c2
Muhammad Raihan <54986720+notepadjdk12@users.noreply.github.com>
2021-09-21 03:30:43 +0700

    Initial commit

---
+++ system/resources/views/Home_V.blade.php
@@ -0,0 +1,26 @@
+@extends('template.base')
+<!-- Header -->
+@section('header')
+@include('template.section_HOME.header_SECTION')  
+@endsection
+<!-- Header -->
+<!-- Page Content -->
+<!-- Banner Starts Here -->
+@section('banner')
+@include('template.section_HOME.banner_SECTION')  
+@endsection
+<!-- Banner Ends Here -->
+<!-- produk -->
+@section('produk')
+@include('template.section_HOME.produk_SECTION')  
+@endsection
+<!-- produk -->    
+    
+    
+   
+    
+    
+    
+    
+    
+    
